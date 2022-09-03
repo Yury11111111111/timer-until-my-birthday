81cf63b21bea8550ba944234b092c659a72ada3a
unknown <yury.ovchinnkov@yandex.ru>
2022-09-03 19:37:49 +0300

    change year

--- src/App.js
+++ src/App.js
@@ -2,20 +2,29 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [date, setDate] = useState(new Date(2022, 7, 9));
+  const [year, setYear] = useState(2023);
+
+  const [date, setDate] = useState(new Date(year, 7, 9));
 
   const [dateNow, setDateNow] = useState(Date.now());
 
   const [time, setTime] = useState(~~((date - dateNow) / 1000));
 
-  const days = Math.floor(time / (60 * 60 * 24))
+  const days = Math.floor(time / (60 * 60 * 24));
   const hours = Math.floor((time / (60 * 60)) % 24);
   const minuts = Math.floor((time / 60) % 60);
   const seconds = time % 60;
 
+  if (time < 0) {
+    setYear(year + 1);
+  }
+
+
+  console.log()
+
   const interval = setInterval(() => {
     setDateNow(Date.now());
-    clearInterval(interval)
+    clearInterval(interval);
     setTime(~~((date - dateNow) / 1000));
   }, 990);
 
