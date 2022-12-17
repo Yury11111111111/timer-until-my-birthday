12f1459d62f5e75a02519c7f6e4af414710b2b66
unknown <yury.ovchinnkov@yandex.ru>
2022-12-17 13:55:30 +0300

    function year change

--- src/App.js
+++ src/App.js
@@ -2,7 +2,10 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [year, setYear] = useState(2023);
+  
+  localStorage.setItem("year", 2023);
+
+  const [year, setYear] = useState(+localStorage.getItem("year"));
 
   const [date, setDate] = useState(new Date(year, 7, 9));
 
@@ -10,19 +13,26 @@ function App() {
 
   const [time, setTime] = useState(~~((date - dateNow) / 1000));
 
-  const days = Math.floor(time / (60 * 60 * 24));
-  const hours = Math.floor((time / (60 * 60)) % 24);
-  const minuts = Math.floor((time / 60) % 60);
-  const seconds = time % 60;
+  const [days, setDays] = useState(Math.floor(time / (60 * 60 * 24)));
+  const [hours, setHours] = useState(Math.floor((time / (60 * 60)) % 24));
+  const [minuts, setMinuts] = useState(Math.floor((time / 60) % 60));
+  const [seconds, setSeconds] = useState(time % 60);
 
-  if (time < 0) {
-    setYear(year + 1);
-  }
-
-
-  console.log()
+  const changeYear = setInterval(() => {
+    clearInterval(changeYear);
+    if (time < 0) {
+      localStorage.setItem("year", () => year + 1);
+      setYear(+localStorage.getItem("year"));
+      setDate(new Date(year, 7, 9));
+      setTime(~~((date - dateNow) / 1000));
+    }
+  }, 1_000);
 
   const interval = setInterval(() => {
+    setDays(Math.floor(time / (60 * 60 * 24)));
+    setHours(Math.floor((time / (60 * 60)) % 24));
+    setMinuts(Math.floor((time / 60) % 60));
+    setSeconds(time % 60);
     setDateNow(Date.now());
     clearInterval(interval);
     setTime(~~((date - dateNow) / 1000));
